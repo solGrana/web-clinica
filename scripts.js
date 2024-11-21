39fe30bf8c7dc2870ec5ebdac44c5bce5bb69d26
sol <sol.grana13@gmail.com>
2024-11-21 17:30:29 -0300

    Agrega boton para el header en pantallas chicas, mejora responsividad

--- scripts.js
+++ scripts.js
@@ -124,3 +124,19 @@ function resetInactivityTimer() {
 
 // Inicializar el temporizador al cargar la pag
 resetInactivityTimer();
+
+// Selecciona los botones y el menú lateral
+const hamburger = document.querySelector('.hamburger');
+const closeBtn = document.querySelector('.close-btn');
+const sideMenu = document.querySelector('.side-menu');
+
+// Cuando se haga clic en la hamburguesa, abrir el menú lateral
+hamburger.addEventListener('click', () => {
+  sideMenu.classList.add('open');
+});
+
+// Cuando se haga clic en la cruz, cerrar el menú lateral
+closeBtn.addEventListener('click', () => {
+  sideMenu.classList.remove('open');
+});
+
